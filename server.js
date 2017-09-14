3a55ccb6b1bf925fee6a49683de3cc36fc6c1128
Codinghunterr <arjunyadav15597@gmail.com>
2017-09-14 16:29:27 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var app = express();
+var hbs = require("hbs");
+const fs = require('fs');
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+hbs.registerHelper('getcurrentyear',function(){
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamit',function(text){
+  return text.toUpperCase();
+});
+
+app.set("view engine","hbs")
+
+
+app.use(function(req,res,next){
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log',log + '\n',function(err){
+    if(err){
+      console.log('unable to append');
+    }
+  });
+  next();
+});
+
+
+app.use(function(req,res,next){
+  res.render("maintenance");
+});
+
+app.use(express.static(__dirname+'/public'));
+
+app.get("/",function(req,res){
+  res.render("home",
+                    {pagetitle:"home page",
+                     welcomemessage: "welcome to my website"
+                   });
+});
+app.get("/about",function(req,res){
+  res.render("about",{pagetitle: "about page",
+                      pagebody: "xyz"});
+});
+app.listen(3000);
